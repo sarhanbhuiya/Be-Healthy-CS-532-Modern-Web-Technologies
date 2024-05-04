4004b01feb00563780042707ae1c291d3ae84c41
Redwan Ul Haq Choyon <redwan@redwan6a31.nmsu.edu>
2024-05-04 12:34:09 -0600

    All the files(ejs) under node.js server and routing is completed, mongodb connection works

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
 
-//const connectDB = require('./server/config/db');
+const connectDB = require('./server/routes/config/db');
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
 //Connect to database
-//connectDB();
+connectDB();
 
 app.use(express.static('public'));
 
--- server/routes/config/db.js
+++ server/routes/config/db.js
@@ -1,4 +1,4 @@
-/*
+
 const mongoose = require('mongoose');
 const connectDB = async () => {
   
@@ -14,4 +14,4 @@ const connectDB = async () => {
 }
 
 module.exports = connectDB;
-*/
+
